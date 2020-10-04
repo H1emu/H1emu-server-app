92b29ca3ba856ab8b1ed0b8b4891141a45fe632f
Quentin Gruber <quentingruber@gmail.com>
2020-10-04 21:04:19 +0200

    Updt info

--- H1Z1-server/Properties/AssemblyInfo.cs
+++ H1Z1-server/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("H1Z1_server")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("H1Z1-NewHope")]
+[assembly: AssemblyDescription("Desktop app to run h1z1 servers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("H1Z1_server")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Gruber Quentin")]
+[assembly: AssemblyProduct("H1Z1-NewHope")]
+[assembly: AssemblyCopyright("Copyright (C) 2020  Quentin Gruber")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
