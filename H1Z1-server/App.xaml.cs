c1634a379695f2e76a2784d7177339890c8dfd29
Quentin Gruber <quentingruber@gmail.com>
2020-10-04 18:57:25 +0200

    Change app from electron to C#

---
+++ H1Z1-server/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace H1Z1_server
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ H1Z1-server/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace H1Z1_server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LaunchServer_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process p = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.RedirectStandardInput = true;
+            info.UseShellExecute = false;
+
+            p.StartInfo = info;
+            p.Start();
+
+            using (StreamWriter sw = p.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    sw.WriteLine("cd h1z1-server-QuickStart/");
+                    sw.WriteLine("npm start");
+                }
+            }
+        }
+
+        private void UpdtServer_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process p = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.RedirectStandardInput = true;
+            info.UseShellExecute = false;
+
+            p.StartInfo = info;
+            p.Start();
+
+            using (StreamWriter sw = p.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    sw.WriteLine("cd h1z1-server-QuickStart/");
+                    sw.WriteLine("npm update");
+                }
+            }
+        }
+        
+        private void InstallServer_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process p = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.RedirectStandardInput = true;
+            info.UseShellExecute = false;
+
+            p.StartInfo = info;
+            p.Start();
+
+            using (StreamWriter sw = p.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    sw.WriteLine("git clone https://github.com/QuentinGruber/h1z1-server-QuickStart.git");
+                    sw.WriteLine("cd h1z1-server-QuickStart/");
+                    sw.WriteLine("npm ci");
+                }
+            }
+        }
+    }
+}
