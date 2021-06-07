b0a7ab31da4fa35e3edab39922f86562cefba2c2
Quentin Gruber <quentingruber@gmail.com>
2021-06-07 21:14:05 +0200

    WIP calculate Crc32 of h1z1.exe

--- H1Emu/Main.cs
+++ H1Emu/Main.cs
@@ -10,9 +10,111 @@ using System.Reflection;
 using Newtonsoft.Json;
 using System.Net;
 using System.Drawing.Text;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace H1Emu
 {
+    public sealed class Crc32 : HashAlgorithm
+    {
+        public const UInt32 DefaultPolynomial = 0xedb88320u;
+        public const UInt32 DefaultSeed = 0xffffffffu;
+
+        static UInt32[] defaultTable;
+
+        readonly UInt32 seed;
+        readonly UInt32[] table;
+        UInt32 hash;
+
+        public Crc32()
+            : this(DefaultPolynomial, DefaultSeed)
+        {
+        }
+
+        public Crc32(UInt32 polynomial, UInt32 seed)
+        {
+            if (!BitConverter.IsLittleEndian)
+                throw new PlatformNotSupportedException("Not supported on Big Endian processors");
+
+            table = InitializeTable(polynomial);
+            this.seed = hash = seed;
+        }
+
+        public override void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            hash = CalculateHash(table, hash, array, ibStart, cbSize);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            var hashBuffer = UInt32ToBigEndianBytes(~hash);
+            HashValue = hashBuffer;
+            return hashBuffer;
+        }
+
+        public override int HashSize { get { return 32; } }
+
+        public static UInt32 Compute(byte[] buffer)
+        {
+            return Compute(DefaultSeed, buffer);
+        }
+
+        public static UInt32 Compute(UInt32 seed, byte[] buffer)
+        {
+            return Compute(DefaultPolynomial, seed, buffer);
+        }
+
+        public static UInt32 Compute(UInt32 polynomial, UInt32 seed, byte[] buffer)
+        {
+            return ~CalculateHash(InitializeTable(polynomial), seed, buffer, 0, buffer.Length);
+        }
+
+        static UInt32[] InitializeTable(UInt32 polynomial)
+        {
+            if (polynomial == DefaultPolynomial && defaultTable != null)
+                return defaultTable;
+
+            var createTable = new UInt32[256];
+            for (var i = 0; i < 256; i++)
+            {
+                var entry = (UInt32)i;
+                for (var j = 0; j < 8; j++)
+                    if ((entry & 1) == 1)
+                        entry = (entry >> 1) ^ polynomial;
+                    else
+                        entry >>= 1;
+                createTable[i] = entry;
+            }
+
+            if (polynomial == DefaultPolynomial)
+                defaultTable = createTable;
+
+            return createTable;
+        }
+
+        static UInt32 CalculateHash(UInt32[] table, UInt32 seed, IList<byte> buffer, int start, int size)
+        {
+            var hash = seed;
+            for (var i = start; i < start + size; i++)
+                hash = (hash >> 8) ^ table[buffer[i] ^ hash & 0xff];
+            return hash;
+        }
+
+        static byte[] UInt32ToBigEndianBytes(UInt32 uint32)
+        {
+            var result = BitConverter.GetBytes(uint32);
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(result);
+
+            return result;
+        }
+    }
     public partial class Main : MaterialForm
     {
         PrivateFontCollection pfc = new PrivateFontCollection();
@@ -48,6 +150,16 @@ namespace H1Emu
             {
                 MessageBox.Show("Please put H1Emu app inside of the H1Z1 Directory.", "H1Emu", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                Crc32 crc32 = new Crc32();
+                String hash = String.Empty;
+
+                using (FileStream fs = File.Open("./h1z1.exe", FileMode.Open))
+                    foreach (byte b in crc32.ComputeHash(fs)) hash += b.ToString("x2").ToLower();
+
+                Console.WriteLine("CRC-32 is {0}", hash);
             }
 
             Stream fontStream = this.GetType().Assembly.GetManifestResourceStream("H1Emu.Carrinady.ttf");
